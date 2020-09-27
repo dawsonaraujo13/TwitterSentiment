dc66f32bf1ac19df17d6f47faa649e75e03f1f51
Dawson Neves de Araujo <dawsonaraujo13@gmail.com>
2020-09-27 00:50:57 -0400

    Finalized streaming

--- twitter_analyzer.py
+++ twitter_analyzer.py
@@ -2,31 +2,66 @@ import tweepy
 from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
+from tweepy import Cursor
 
 import twitter_credentials
 
 
+class TStreamer():
+    
+    # This class is used to stream and process live tweets
+    def __init__(self):
+        self.twitter_authenticator = TAuthentication()
+
+    def stream_tweets(self, captured_tweets_filename, keyword_list):
+        
+        listener = StdOutTweetListener(captured_tweets_filename)
+        auth = self.twitter_authenticator.authenticate_twitter()
+        stream = Stream(auth,listener)
+        stream.filter(track = keyword_list)
+
+
+class TAuthentication():
+
+    # This class handles authentification for the Twitter API
+
+
+    def authenticate_twitter(self):
+        auth = tweepy.OAuthHandler(twitter_credentials.API_KEY, twitter_credentials.API_KEY_SECRET)
+        auth.set_access_token(twitter_credentials.ACCESS_KEY, twitter_credentials.ACCESS_KEY_SECRET)
+        return auth
+
 class StdOutTweetListener(StreamListener):
 
+    # Class that prints out received tweets
+
+    def __init__(self, captured_tweets_filename):
+        self.captured_tweets_filename = captured_tweets_filename
+
     def on_data(self, data):
-        print(data)
+
+        try:
+            print(data)
+            with open(self.captured_tweets_filename, 'a') as tf:
+                tf.write(data)
+            return True
+        except BaseException as e:
+            print("Error on_data %s" % str(e))
         return True
 
     def on_error(self,status):
-        print(status)
+        if status == 420:
+            return False
 
 
 if __name__ == "__main__":
 
-    listener = StdOutTweetListener()
-    auth = tweepy.OAuthHandler(twitter_credentials.API_KEY, twitter_credentials.API_KEY_SECRET)
-    auth.set_access_token(twitter_credentials.ACCESS_KEY, twitter_credentials.ACCESS_KEY_SECRET)
+    keyword_list = ['Donald Trump', 'Joe Biden']
+    captured_tweets_filename = "tweets.json"
 
-    api = tweepy.API(auth)
+    twitter_stream = TStreamer()
+    twitter_stream.stream_tweets(captured_tweets_filename, keyword_list)
 
-    stream = Stream(auth,listener)
-
-    stream.filter(track = ['AMD'])
         
     
 
